910095894e8cd8c7d6c8af30048a1c4d8830b4ad
ShehabAlden <shosho2015.sm93@gmail.com>
2024-03-06 00:20:58 +0200

    modified index.js & package.json for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import application from './app/app.js'
 const app = express()
-const port = 3000
+const port = +process.env.PORT
 application(app)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
