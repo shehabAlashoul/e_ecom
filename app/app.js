f3a6f45ed5ae4bd61d8c65ddbafc96c1179a558a
ShehabAlden <shosho2015.sm93@gmail.com>
2024-03-06 22:10:13 +0200

    last

--- app/app.js
+++ app/app.js
@@ -10,38 +10,33 @@ import { AppError } from "./utils/error.handler.js";
 dotenv.config();
 const application = (app) => {
 
-  app.post(
-    "/webhook",
-    express.raw({ type: "application/json" }),
-    (request, response) => {
-      const sig = request.headers["stripe-signature"];
-
-      let event;
-      console.log('shosho fffffffff');
-
-      try {
-        event = stripe.webhooks.constructEvent(
-          request.body,
-          sig,
-          process.env.STRIPE_WEBHOOK_SECRET
-        );
-      } catch (err) {
-        response.status(400).send(`Webhook Error: ${err.message}`);
-        return;
-      }
-      switch (event.type) {
-        case "checkout.session.completed":
-          const data = event.data.object;
-          console.log({data});
-          break;
-        default:
-          console.log(`Unhandled event type ${event.type}`);
-      }
-
-      
-      response.send();
+  app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    const sig = request.headers['stripe-signature'];
+  
+    let event;
+  
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
     }
-  );
+  
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const checkoutSessionCompleted = event.data.object;
+        console.log({checkoutSessionCompleted});
+        // Then define and call a function to handle the event checkout.session.completed
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  });
 
   // app.listen(4242, () => console.log("Running on port 4242"));
   app.use(express.json());
