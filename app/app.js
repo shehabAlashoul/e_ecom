30375b961901d22dc699674a1cafb8dc9a480caa
ShehabAlden <shosho2015.sm93@gmail.com>
2024-03-06 16:08:32 +0200

    add webhook for online payment

--- app/app.js
+++ app/app.js
@@ -8,9 +8,45 @@ import { AppError } from "./utils/error.handler.js";
 
 const application = (app) => {
   dotenv.config();
+
+  app.post(
+    "/webhook",
+    express.raw({ type: "application/json" }),
+    (request, response) => {
+      const sig = request.headers["stripe-signature"];
+
+      let event;
+
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          sig,
+          process.env.STRIPE_WEBHOOK_SECRET
+        );
+      } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+
+      // Handle the event
+      switch (event.type) {
+        case "checkout.session.completed":
+          const data = event.data.object;
+          console.log(data);
+          break;
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
+
+      
+      response.send();
+    }
+  );
+
+  app.listen(4242, () => console.log("Running on port 4242"));
   app.use(express.json());
   connectToDB();
-  app.use(morgan('dev'))
+  app.use(morgan("dev"));
   app.use("/ecom", appRouter);
   // _________________________________________ cloudnairy config ____________________________________________
   cloudinary.config({
